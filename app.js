fbc9fdf0756f438aa2cf6da054da2a4bcbe0f116
Siphelele Shabalala <sipheleleshabalala5@gmail.com>
2025-02-04 18:03:46 +0200

    added nodemon to automate server initialisation

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res) => {
-    res.send("Hello From The Server");
+    res.send("Hello From The Node Server");
 });
 app.get("/cars", (req,res) => {
     res.send("This Is For For Cars")
 });
 
 app.listen(8000, () => {
-    console.log("Server Connected Successfully!")
+    console.log("Server Has Connected Successfully!")
 });
